4dff28dd7790fd23ce4fa02db504cdd49d881739
vladbelcar <vladstart1@hotmail.com>
2024-07-12 01:06:00 +0300

    Check commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Updated text
+            Test
           </p>
         </a>
 
